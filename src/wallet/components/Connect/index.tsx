6cebac87c2ae04165142bef0c45332fe73b37606
Mateus Dal Bianco <mateus@dlbn.co>
2022-10-31 14:24:06 -0300

    feat(Connect): tweak hook deps; include form element

--- src/wallet/components/Connect/index.tsx
+++ src/wallet/components/Connect/index.tsx
@@ -2,13 +2,15 @@ import { useSessionStorageValue } from "@react-hookz/web";
 import { Box, Button, Input, Label, Text } from "@theme-ui/components";
 import Wallet from "ethereumjs-wallet";
 import { useRouter } from "next/router";
-import React, { useCallback, useState } from "react";
+import React, { FormEvent, useCallback, useState } from "react";
 
 const Connect: React.FC = () => {
   const router = useRouter();
   const [input, setInput] = useState<string>("");
-  const [sessionPrivateKey, setSessionPrivateKey] =
-    useSessionStorageValue<string>("privateKey", "");
+  const [, setSessionPrivateKey] = useSessionStorageValue<string>(
+    "privateKey",
+    ""
+  );
   const [error, setError] = useState<any>(null);
 
   const onChangeInput = useCallback(
@@ -16,44 +18,50 @@ const Connect: React.FC = () => {
       if (error != null) setError(null);
       setInput(e.target.value);
     },
-    [setSessionPrivateKey, error]
+    [setInput, error]
   );
 
-  const onClick = useCallback(() => {
-    try {
-      /**
-       * Tries to initialize the wallet before proceeding;
-       * Throws if there's something wrong with the private key.
-       */
-      const wallet = Wallet.fromPrivateKey(Buffer.from(input, "hex"));
-      setSessionPrivateKey(input);
-      router.push(`/wallet?address=${wallet.getAddressString()}`);
-    } catch (e) {
-      console.error(e);
-      setError(e);
-    }
-  }, [sessionPrivateKey, router]);
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      try {
+        /**
+         * Tries to initialize the wallet before proceeding;
+         * Throws if there's something wrong with the private key.
+         */
+        const wallet = Wallet.fromPrivateKey(Buffer.from(input, "hex"));
+        setSessionPrivateKey(input);
+        router.push(`/wallet?address=${wallet.getAddressString()}`);
+      } catch (e) {
+        setError(e);
+      }
+    },
+    [router, input, setSessionPrivateKey]
+  );
 
   return (
     <Box>
-      <Label sx={{ flexDirection: "column" }} mb={2}>
-        <Text mb={1} sx={{ display: "block" }}>
-          Private key
-        </Text>
-        <Input
-          value={input}
-          onChange={onChangeInput}
-          sx={{ display: "block" }}
-        />
-      </Label>
-      {error?.message != null && (
-        <Text sx={{ display: "block" }} my={2}>
-          {error.message}
-        </Text>
-      )}
-      <Button mb={3} onClick={onClick}>
-        Connect
-      </Button>
+      <Box as="form" onSubmit={onSubmit}>
+        <Label sx={{ flexDirection: "column" }} mb={2}>
+          <Text mb={1} sx={{ display: "block" }}>
+            Private key
+          </Text>
+          <Input
+            value={input}
+            onChange={onChangeInput}
+            sx={{ display: "block" }}
+            data-testid="privateKey-input"
+          />
+        </Label>
+        {error?.message != null && (
+          <Text sx={{ display: "block" }} my={2} data-testid="error-message">
+            {error.message}
+          </Text>
+        )}
+        <Button mb={3} type="submit" data-testid="submit-button">
+          Connect
+        </Button>
+      </Box>
       <Text variant="secondary" sx={{ display: "block", fontSize: 0 }}>
         Your data will be stored in your device only
       </Text>
